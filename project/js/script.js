4e4b7ca41acd51b4f68a7976dbf667cc5eedb836
NickDevt <48755888+NickDevt@users.noreply.github.com>
2019-11-26 18:24:27 +0300

    После экспорта

--- project/js/script.js
+++ project/js/script.js
@@ -2,3 +2,18 @@
 // $("#my_image").bind("click", function() {
 //       $("#my_image").attr("src","second.jpg");
 // });
+
+
+
+// $(document).ready(function() {
+// 	$('.accordion').on('click', '.faq-btn', function() {
+// 		$(this).toggleClass('greyer').siblings('.whiter').slideToggle(0);
+// 	});
+// });
+
+
+$(document).ready(function(){
+ $('.whiter').click(function () {
+   $(this).toggleClass('greyer');
+   });
+ });
